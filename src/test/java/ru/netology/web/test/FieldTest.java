3b2f7b789e9046f9810e0859eeeac478249d4306
Veragen <veragen@mail.ru>
2022-06-28 15:16:35 +0700

    for my check

--- src/test/java/ru/netology/web/test/FieldTest.java
+++ src/test/java/ru/netology/web/test/FieldTest.java
@@ -2,25 +2,39 @@ package ru.netology.web.test;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.PaymentPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FieldTest {
     @Test
     void ShouldOpenAndPaste() {
-open("http://localhost:8080");
-var dashboardPage = new DashboardPage();
-var paymentPage = new PaymentPage();
-dashboardPage.clickOnButtonPayCard();
-dashboardPage.clickOnButtonCreditCard();
-paymentPage.validCard();
-paymentPage.checkAlarm();
-//paymentPage.checkAlarm();
-
+        open("http://localhost:8080");
+        var dashboardPage = new DashboardPage();
+        var paymentPage = new PaymentPage();
+        dashboardPage.clickOnButtonPayCard();
+        dashboardPage.clickOnButtonCreditCard();
+        paymentPage.validCard();
+        paymentPage.checkAlarm();
 
 
     }
 
+  //  class BonusServiceTest {
+ //       @ParameterizedTest
+  //     @CsvSource(value = {"'registered user, under limit',100060,true,30",
+ //              "'registered user, bonus under limit',100000060,true,500"})
+  //     void shouldCalculate(String testName, long amount, boolean registered, long expected) {
+//            BonusService service = new BonusService();
+//            long actual = service.calculate(amount, registered);
+//
+//            assertEquals(expected, actual);
+//        }
+
+
+   // }
 }
